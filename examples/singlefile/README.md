455682144521128901a36b56ea52518136791c1b
Nejc Zupan <nejc.zupan@gmail.com>
2019-04-17 21:03:45 +0100

    Add a slightly more built-out TODO app example
    Based on https://github.com/zupo/minimal_openapi which is now rendered
    obsolete.
    
    Also removed demo.py and demo.yaml as they are no longer needed.
    
    Finally, removed the `create https://www.todobackend.com/ implementation`
    TODO since this project is sadly long forgotten.

---
+++ examples/singlefile/README.md
@@ -0,0 +1,34 @@
+# An single-file example RESTful API app showcasing the power of pyramid_openapi3
+
+The `app.py` file in this folder showcases how to use the [pyramid_openapi3](https://github.com/niteoweb/pyramid_openapi3) Pyramid add-on for building robust RESTful APIs. With only a few lines of code you get automatic validation of requests and responses against an OpenAPI v3 schema, along with Swagger "try-it-out" documentation for your API.
+
+## How to run
+
+```bash
+* git clone https://github.com/niteoweb/pyramid_openapi3.git
+* cd pyramid_openapi3/examples/singlefile
+* virtualenv -p python3.7 .
+* source bin/activate
+* pip install pyramid_openapi3
+* python app.py
+```
+
+Then use the Swagger interface at http://localhost:6543/docs/ to discover the API. Use the `Try it out` button to run through a few request/response scenarios.
+
+For example:
+* Say Hello by doing a GET request to `http://localhost:6543/hello?name=john`.
+* Get an Exception raised if you omit the required `name` query parameter.
+* Get an Exception raised if the query parameter `name` is too short.
+
+All of these examples are covered with tests that you can run with `$ python -m unittest app.py`.
+
+
+## Further read
+
+* A slightly more complex, multi-file example is available in the [`examples/todoapp`](https://github.com/niteoweb/pyramid_openapi3/tree/master/examples/todoapp) folder.
+
+* More information about the library providing the integration between OpenAPI specs and Pyramid, more advanced features and design defence, is available in the main [README](https://github.com/niteoweb/pyramid_openapi3) file.
+
+* More validators for fields are listed in the [OpenAPI Specification](https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#properties) document. You can use Regex as well.
+
+* For an idea of a fully-fledged production OpenApi specification, check out [WooCart's OpenAPI docs](https://app.woocart.com/api/v1/).
---
+++ examples/todoapp/tests.py
@@ -0,0 +1,63 @@
+"""A couple of functional tests to showcase pyramid_openapi3 features."""
+
+from webtest import TestApp
+
+import app
+import unittest
+
+
+class TestHappyPath(unittest.TestCase):
+    """A suite of tests that make "happy path" requests against the app."""
+
+    def setUp(self):
+        """Start up the app so that tests can send requests to it."""
+        self.testapp = TestApp(app.app())
+
+    def test_list_todos(self):
+        """Root returns a list of TODOs."""
+        res = self.testapp.get("/", status=200)
+        self.assertEqual(
+            res.json,
+            [{"title": "Buy milk"}, {"title": "Buy eggs"}, {"title": "Make pankaces!"}],
+        )
+
+    def test_add_todo(self):
+        """POSTing to root saves a TODO."""  # noqa: D403
+        res = self.testapp.post_json("/", {"title": "Add marmalade"}, status=200)
+        self.assertEqual(res.json, "Item added.")
+
+        # clean up after the test by removing the "Add marmalade" item
+        app.ITEMS.pop()
+
+
+class TestBadRequests(TestHappyPath):
+    """A suite of tests that showcase out-of-the-box handling of bad requests."""
+
+    def test_empty_POST(self):
+        """Get a nice validation error when sending an empty POST request."""
+        res = self.testapp.post_json("/", {}, status=400)
+        self.assertEqual(
+            res.json,
+            [
+                {
+                    "exception": "MissingSchemaProperty",
+                    "field": "title",
+                    "message": "Missing schema property: title",
+                }
+            ],
+        )
+
+    def test_title_too_long(self):
+        """Get a nice validation error when title is too long."""
+        res = self.testapp.post_json("/", {"title": "a" * 41}, status=400)
+        self.assertEqual(
+            res.json,
+            [
+                {
+                    "exception": "InvalidSchemaProperty",
+                    "field": "title",
+                    "message": "Invalid schema property title: "
+                    "Value is longer (41) than the maximum length of 40",
+                }
+            ],
+        )
