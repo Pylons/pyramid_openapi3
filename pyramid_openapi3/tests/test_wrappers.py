54e8739ba02ab7aa8682674ea913114c0ac9a1cc
Wim De Clercq <declewi@gmail.com>
2021-05-17 17:03:36 +0200

    Add simple form-data support.
    Also fixed broken tests.
    
    Issue #122

--- pyramid_openapi3/tests/test_wrappers.py
+++ pyramid_openapi3/tests/test_wrappers.py
@@ -1,12 +1,14 @@
 """Tests for the wrappers.py module."""
 
 from dataclasses import dataclass
+
 from openapi_core.validation.request.datatypes import RequestParameters
 from pyramid.request import Request
 from pyramid.testing import DummyRequest
+from webob.multidict import MultiDict
+
 from pyramid_openapi3.wrappers import PyramidOpenAPIRequestFactory
 from pyramid_openapi3.wrappers import PyramidOpenAPIResponseFactory
-from pyramid_openapi3.wrappers import request_headers
 
 
 @dataclass
@@ -66,9 +68,27 @@ def test_relative_app_request() -> None:
     assert openapi_request.parameters == RequestParameters(
         path={"foo": "bar"},
         query={},
-        header=request_headers(pyramid_request),
+        header=pyramid_request.headers.items(),
         cookie={"tasty-foo": "tasty-bar"},
     )
+
+
+def test_form_data_request() -> None:
+    """Test that request.POST is used as the body in case of form-data."""
+    multi_dict = MultiDict()
+    multi_dict.add('key1', 'value1')
+    multi_dict.add('key2', 'value2.1')
+    multi_dict.add('key2', 'value2.2')
+    pyramid_request = DummyRequest(path="/foo", post=multi_dict)
+    pyramid_request.matched_route = DummyRoute(name="foo", pattern="/foo")
+    pyramid_request.content_type = "multipart/form-data"
+
+    openapi_request = PyramidOpenAPIRequestFactory.create(pyramid_request)
+
+    assert openapi_request.body == {
+        'key1': 'value1',
+        'key2': ['value2.1', 'value2.2']
+    }
 
 
 def test_no_matched_route() -> None:
