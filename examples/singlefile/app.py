9f4aa8074c6e312482670bd818f71d017df0828b
Pepijn Verlaan <pepijn@motosumo.com>
2022-05-17 15:36:41 +0200

    Demonstrate x-www-form-urlencoded issue in singlefile example

--- examples/singlefile/app.py
+++ examples/singlefile/app.py
@@ -20,6 +20,15 @@ OPENAPI_DOCUMENT = b"""
     info:
       version: "1.0.0"
       title: Hello API
+    components:
+      schemas:
+        ObjectWithName:
+          type: object
+          properties:
+            name:
+              type: string
+          required:
+            - name
     paths:
       /hello:
         get:
@@ -35,12 +44,34 @@ OPENAPI_DOCUMENT = b"""
               description: Say hello
             400:
               description: Bad Request
+        post:
+          requestBody:
+            required: true
+            content:
+              application/json:
+                schema:
+                  $ref: "#/components/schemas/ObjectWithName"
+              application/x-www-form-urlencoded:
+                schema:
+                  $ref: "#/components/schemas/ObjectWithName"
+          responses:
+            200:
+              description: Say hello
+            400:
+              description: Bad Request
 """
 
 
-@view_config(route_name="hello", renderer="json", request_method="GET", openapi=True)
+@view_config(
+    route_name="hello", renderer="json", request_method=("GET", "POST"), openapi=True
+)
 def hello(request):
     """Say hello."""
+    # This implementation is a bit weird, but I could only get webtest to work if there was a
+    # single view function handling the route.
+    if request.method == "POST":
+        return {"hello_post": request.openapi_validated.body["name"]}
+
     if request.openapi_validated.parameters["query"]["name"] == "admin":
         raise HTTPForbidden()
     return {"hello": request.openapi_validated.parameters["query"]["name"]}
@@ -52,6 +83,16 @@ def app(spec):
         config.include("pyramid_openapi3")
         config.pyramid_openapi3_spec(spec)
         config.pyramid_openapi3_add_explorer()
+
+        # This is a workaround for the issue:
+        #
+        # from urllib.parse import parse_qsl
+        #
+        # config.pyramid_openapi3_add_deserializer(
+        #     "application/x-www-form-urlencoded",
+        #     lambda body_bytes: dict(parse_qsl(body_bytes.decode(encoding="ascii"))),
+        # )
+
         config.add_route("hello", "/hello")
         config.scan(".")
         return config.make_wsgi_app()
@@ -103,6 +144,37 @@ class FunctionalTests(unittest.TestCase):
         """Say hello."""
         res = self.testapp.get("/hello?name=john", status=200)
         self.assertIn('{"hello": "john"}', res.text)
+
+    def test_hello_post_json(self):
+        """Say hello to a json POST."""
+        res = self.testapp.post_json("/hello", {"name": "jane"}, status=200)
+        self.assertIn('{"hello_post": "jane"}', res.text)
+
+    def test_hello_post_form(self):
+        """Say hello to a form POST."""
+        # Make the request but don't assert the status immediately
+        res = self.testapp.post("/hello", params={"name": "joe"}, status="*")
+        # First (and for our own sanity) assert the request is actually what we expect it to be
+        self.assertEqual(
+            "POST /hello HTTP/1.0\r\n"
+            "Content-Length: 8\r\n"
+            "Content-Type: application/x-www-form-urlencoded\r\n"
+            "Host: localhost:80\r\n"
+            "\r\n"
+            "name=joe",
+            str(res.request),
+        )
+        # Then assert the status, this fails, status_code is 400
+        self.assertEqual(200, res.status_code)
+        # And this also fails, response text is:
+        #   [
+        #     {
+        #       "exception": "ValidationError",
+        #       "message": "'name' is a required property",
+        #       "field": "name"
+        #     }
+        #   ]
+        self.assertIn('{"hello_post": "joe"}', res.text)
 
     def test_undefined_response(self):
         """Saying hello to admin should fail with 403 Forbidden.
