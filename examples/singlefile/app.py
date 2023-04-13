941b967cfdc51d149fe6dd11221143b68c5f1b9e
Nejc Zupan <nejc.zupan@gmail.com>
2023-04-13 22:54:28 +0100

    Showcase a problem with using v3.1 multiple types with format and null

--- examples/singlefile/app.py
+++ examples/singlefile/app.py
@@ -5,7 +5,6 @@ https://github.com/Pylons/pyramid_openapi3/tree/main/examples/singlefile
 """
 
 from pyramid.config import Configurator
-from pyramid.httpexceptions import HTTPForbidden
 from pyramid.view import view_config
 from wsgiref.simple_server import make_server
 
@@ -21,29 +20,27 @@ OPENAPI_DOCUMENT = b"""
       version: "1.0.0"
       title: Hello API
     paths:
-      /hello:
+      /today:
         get:
-          parameters:
-            - name: name
-              in: query
-              required: true
-              schema:
-                type: string
-                minLength: 3
           responses:
             200:
               description: Say hello
-            400:
-              description: Bad Request
+              content:
+                application/json:
+                  schema:
+                    type: object
+                    properties:
+                      today:
+                        type: [string, "null"]
+                        format: date
+
 """
 
 
-@view_config(route_name="hello", renderer="json", request_method="GET", openapi=True)
-def hello(request):
-    """Say hello."""
-    if request.openapi_validated.parameters.query["name"] == "admin":
-        raise HTTPForbidden()
-    return {"hello": request.openapi_validated.parameters.query["name"]}
+@view_config(route_name="today", renderer="json", request_method="GET", openapi=True)
+def today(request):
+    """Return today's date or None."""
+    return {"today": None}
 
 
 def app(spec):
@@ -52,7 +49,7 @@ def app(spec):
         config.include("pyramid_openapi3")
         config.pyramid_openapi3_spec(spec)
         config.pyramid_openapi3_add_explorer()
-        config.add_route("hello", "/hello")
+        config.add_route("today", "/today")
         config.scan(".")
         return config.make_wsgi_app()
 
@@ -89,46 +86,7 @@ class FunctionalTests(unittest.TestCase):
 
             self.testapp = TestApp(app(document.name))
 
-    def test_nothing_on_root(self):
-        """We have not configured our app to serve anything on root."""
-        res = self.testapp.get("/", status=404)
-        self.assertIn("404 Not Found", res.text)
-
-    def test_api_explorer_on_docs(self):
-        """Swagger's API Explorer should be served on /docs/."""
-        res = self.testapp.get("/docs/", status=200)
-        self.assertIn("<title>Swagger UI</title>", res.text)
-
-    def test_hello(self):
-        """Say hello."""
-        res = self.testapp.get("/hello?name=john", status=200)
-        self.assertIn('{"hello": "john"}', res.text)
-
-    def test_undefined_response(self):
-        """Saying hello to admin should fail with 403 Forbidden.
-
-        But because we have not defined how a 403 response should look in
-        OPENAPI_DOCUMENT, we instead get an error 500 response, because it is
-        the servers fault to generate an invalid response.
-
-        This is to prevent us from forgetting to define all possible responses
-        in our openapi document.
-        """
-        res = self.testapp.get("/hello?name=admin", status=500)
-        self.assertIn("Unknown response http status: 403", res.text)
-
-    def test_name_missing(self):
-        """Our view code does not even get called is request is not per-spec.
-
-        We don't have to write (and test!) any validation code in our view!
-        """
-        res = self.testapp.get("/hello", status=400)
-        self.assertIn("Missing required query parameter: name", res.text)
-
-    def test_name_too_short(self):
-        """A name that is too short is picked up by openapi-core validation.
-
-        We don't have to write (and test!) any validation code in our view!
-        """
-        res = self.testapp.get("/hello?name=yo", status=400)
-        self.assertIn("'yo' is too short", res.text)
+    def test_today(self):
+        """Say today."""
+        res = self.testapp.get("/today", status=200)
+        self.assertIn('{"today": null}', res.text)
